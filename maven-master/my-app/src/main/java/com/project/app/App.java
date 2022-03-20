c3eb49581b8dc4dedefd804a456de978acfe4764
Ruthrojasayala <95321129+Ruthrojasayala@users.noreply.github.com>
2022-03-20 14:41:06 -0500

    first commit

---
+++ maven-master/my-app/src/main/java/com/project/app/App.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int number='53'
+        System.out.println( "Jenkins DevOps! " + number);
+         try {
+            for ( int i = number; i < number*2; i++ ){
+                Thread.sleep(1000);
+                System.out.println( "Doing " + i);
+            }
+        }
+        catch (Exception e) {
+
+            System.out.println(e);
+        }
+        System.out.println("Finished Activities");
+    }
+}
