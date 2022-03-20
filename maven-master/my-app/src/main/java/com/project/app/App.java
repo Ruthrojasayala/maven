19db5121d653e7c17e91ad3e7e131826f4117d7c
Ruthrojasayala <95321129+Ruthrojasayala@users.noreply.github.com>
2022-03-20 14:51:12 -0500

    delete ''

--- maven-master/my-app/src/main/java/com/project/app/App.java
+++ maven-master/my-app/src/main/java/com/project/app/App.java
@@ -8,12 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        int number='53';
+        int number=53;
         System.out.println( "Jenkins DevOps! " + number);
          try {
             for ( int i = number; i < number*2; i++ ){
                 Thread.sleep(1000);
                 System.out.println( "Doing " + i);
+                for ()
             }
         }
         catch (Exception e) {
